43a2fc52533017ad418525ca62addd831c2aee0a
falx123 <60406217+falx123@users.noreply.github.com>
2022-03-25 14:43:35 +0100

    Added CommandContext to solution

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -8,17 +8,27 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 namespace CommandAPI
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CommandContext>(opt => opt.UseNpgsql
+                (Configuration.GetConnectionString("PostgreSqlConnection")));
+
             services.AddControllers();
-            services.AddScoped<Data.ICommandAPIRepo, MockCommandAPIRepo>();
+            // services.AddScoped<Data.ICommandAPIRepo, MockCommandAPIRepo>();
+            services.AddScoped<Data.ICommandAPIRepo, SqlCommandAPIRepo>();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,7 +44,7 @@ namespace CommandAPI
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-                
+
                 // endpoints.MapGet("/", async context =>
                 // {
                 //     await context.Response.WriteAsync("Hello World!");
